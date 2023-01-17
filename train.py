1fcecb256c2a5fe51ddf1e597514f536cc1b16c4
Maciej Ziaja <mziaja@kplabs.pl>
2023-01-17 16:26:30 +0100

    Add mlflow to train

--- train.py
+++ train.py
@@ -4,14 +4,17 @@ import gc
 # -- Third-part modules -- #
 import json
 import os
+from tempfile import TemporaryDirectory
 
 # -- Environmental variables -- #
 os.environ['AI4ARCTIC_DATA'] = './data/ai4arctic_challenge'  # Fill in directory for data location.
 os.environ['AI4ARCTIC_ENV'] = './'  # Fill in directory for environment with Ai4Arctic get-started package.
 
+import clize
+import mlflow
 import numpy as np
 import torch
-from tqdm.notebook import tqdm  # Progress bar
+from tqdm import tqdm  # Progress bar
 
 # --Proprietary modules -- #
 from functions import (  # Functions to calculate metrics and show the relevant chart colorbar.
@@ -26,24 +29,27 @@ from train_options import TRAIN_OPTIONS
 from unet import UNet  # Convolutional Neural Network model
 from utils import colour_str
 
+MLFLOW_URI = 'http://misc.mlflow.kplabs.pl'
+EXPERIMENT_NAME = 'AI4Artctic challenge'
+
 
 def setup_device(train_options: dict) -> torch.device:
     if torch.has_cuda:
         print(colour_str('GPU available!', 'green'))
         print('Total number of available devices: ', colour_str(torch.cuda.device_count(), 'orange'))
         device = torch.device(f"cuda:{train_options['gpu_id']}")
-    elif torch.has_mps:
-        print(colour_str('MPS available!', 'green'))
-        device = torch.device('mps')
+    # elif torch.has_mps:
+    #     print(colour_str('MPS available!', 'green'))
+    #     device = torch.device('mps')
     else:
-        print(colour_str('GPU not available.', 'red'))
+        print(colour_str('GPU not available', 'red'))
         device = torch.device('cpu')
     return device
 
 
 def setup_options(options: dict) -> dict:
     # Get options for variables, amsrenv grid, cropping and upsampling.
-    train_options = get_variable_options(options)
+    train_options = get_variable_options(options.copy())
 
     # Load training list.
     with open(train_options['path_to_env'] + 'datalists/dataset.json') as file:
@@ -82,7 +88,13 @@ def setup_model(train_options: dict) -> torch.nn.Module:
     return model
 
 
-def main():
+def prepare_train_options_for_logging(train_options: dict) -> dict:
+    ret = train_options.copy()
+    del ret['train_variables']
+    return ret
+
+
+def main(run_name: str, *, remote_mlflow: bool = False):
     train_options = setup_options(TRAIN_OPTIONS)
     print('Options initialised')
 
@@ -98,7 +110,7 @@ def main():
     torch.backends.cudnn.benchmark = (
         True  # Selects the kernel with the best performance for the GPU and given input size.
     )
-    print('Model setup complete.')
+    print('Model setup complete')
 
     # Loss functions to use for each sea ice parameter.
     # The ignore_index argument discounts the masked values, ensuring that the model is not using these pixels to train
@@ -107,105 +119,129 @@ def main():
         chart: torch.nn.CrossEntropyLoss(ignore_index=train_options['class_fill_values'][chart])
         for chart in train_options['charts']
     }
-    print('Model setup complete')
+    print('Training setup complete')
 
     best_combined_score = 0  # Best weighted model score.
 
-    # -- Training Loop -- #
-    for epoch in tqdm(iterable=range(train_options['epochs']), position=0):
-        gc.collect()  # Collect garbage to free memory.
-        loss_sum = torch.tensor([0.0])  # To sum the batch losses during the epoch.
-        net.train()  # Set network to evaluation mode.
+    if remote_mlflow:
+        mlflow.set_tracking_uri(MLFLOW_URI)
 
-        # Loops though batches in queue.
-        for i, (batch_x, batch_y) in enumerate(
-            tqdm(iterable=dataloader, total=train_options['epoch_len'], colour='red', position=0)
-        ):
-            torch.cuda.empty_cache()  # Empties the GPU cache freeing up memory.
-            loss_batch = 0  # Reset from previous batch.
+    mlflow.set_experiment(experiment_name=EXPERIMENT_NAME)
+    with mlflow.start_run(run_name=run_name) as run, TemporaryDirectory() as artifacts_tmp_dir:
+        mlflow.log_params(prepare_train_options_for_logging(TRAIN_OPTIONS))
+        best_model_artifact_path = os.path.join(artifacts_tmp_dir, 'ice_best_model.pt')
 
-            # - Transfer to device.
-            batch_x = batch_x.to(device, non_blocking=True)
+        # -- Training Loop -- #
+        for epoch in tqdm(range(train_options['epochs']), position=0, desc='Epoch'):
+            gc.collect()  # Collect garbage to free memory.
+            loss_sum = torch.tensor([0.0])  # To sum the batch losses during the epoch.
+            net.train()  # Set network to evaluation mode.
 
-            # - Mixed precision training. (Saving memory)
-            with torch.cuda.amp.autocast():
-                # - Forward pass.
-                output = net(batch_x)
+            # Loops though batches in queue.
+            for i, (batch_x, batch_y) in enumerate(
+                (
+                    progress := tqdm(
+                        iterable=dataloader, total=train_options['epoch_len'], colour='red', position=0, desc='Batch'
+                    )
+                )
+            ):
+                torch.cuda.empty_cache()  # Empties the GPU cache freeing up memory.
+                loss_batch = 0  # Reset from previous batch.
 
-                # - Calculate loss.
+                # - Transfer to device.
+                batch_x = batch_x.to(device, non_blocking=True)
+
+                # - Mixed precision training. (Saving memory)
+                with torch.cuda.amp.autocast(enabled=True if device == 'cuda' else False):
+                    # - Forward pass.
+                    output = net(batch_x)
+
+                    # - Calculate loss.
+                    for chart in train_options['charts']:
+                        loss_batch += loss_functions[chart](input=output[chart], target=batch_y[chart].to(device))
+
+                # - Reset gradients from previous pass.
+                optimizer.zero_grad()
+
+                # - Backward pass.
+                loss_batch.backward()
+
+                # - Optimizer step
+                optimizer.step()
+
+                # - Add batch loss.
+                loss_sum += loss_batch.detach().item()
+
+                # - Average loss for displaying
+                loss_epoch = torch.true_divide(loss_sum, i + 1).detach().item()
+                progress.set_description(f'Mean training loss: {loss_epoch:.3f}, batch')
+                mlflow.log_metric('batch_loss', loss_batch, step=epoch * train_options['epoch_len'] + i)
+                mlflow.log_metric('mean_batch_loss_in_epoch', loss_epoch, step=epoch * train_options['epoch_len'] + i)
+                del output, batch_x, batch_y  # Free memory
+            del loss_sum
+
+            # -- Validation Loop -- #
+            loss_batch = loss_batch.detach().item()  # For printing after the validation loop.
+
+            # - Stores the output and the reference pixels to calculate the scores after inference on all the scenes.
+            outputs_flat = {chart: np.array([]) for chart in train_options['charts']}
+            inf_ys_flat = {chart: np.array([]) for chart in train_options['charts']}
+
+            net.eval()  # Set network to evaluation mode.
+            # - Loops though scenes in queue.
+            for inf_x, inf_y, masks, name in tqdm(
+                iterable=dataloader_val,
+                total=len(train_options['validate_list']),
+                colour='green',
+                position=0,
+                desc='Validation',
+            ):
+                torch.cuda.empty_cache()
+
+                # - Ensures that no gradients are calculated, which otherwise take up a lot of space on the GPU.
+                with torch.no_grad(), torch.cuda.amp.autocast(enabled=True if device == 'cuda' else False):
+                    inf_x = inf_x.to(device, non_blocking=True)
+                    output = net(inf_x)
+
+                # - Final output layer, and storing of non masked pixels.
                 for chart in train_options['charts']:
-                    loss_batch += loss_functions[chart](input=output[chart], target=batch_y[chart].to(device))
+                    output[chart] = torch.argmax(output[chart], dim=1).squeeze().cpu().numpy()
+                    outputs_flat[chart] = np.append(outputs_flat[chart], output[chart][~masks[chart]])
+                    inf_ys_flat[chart] = np.append(inf_ys_flat[chart], inf_y[chart][~masks[chart]].numpy())
 
-            # - Reset gradients from previous pass.
-            optimizer.zero_grad()
+                del inf_x, inf_y, masks, output  # Free memory.
 
-            # - Backward pass.
-            loss_batch.backward()
-
-            # - Optimizer step
-            optimizer.step()
-
-            # - Add batch loss.
-            loss_sum += loss_batch.detach().item()
-
-            # - Average loss for displaying
-            loss_epoch = torch.true_divide(loss_sum, i + 1).detach().item()
-            print('\rMean training loss: ' + f'{loss_epoch:.3f}', end='\r')
-            del output, batch_x, batch_y  # Free memory
-        del loss_sum
-
-        # -- Validation Loop -- #
-        loss_batch = loss_batch.detach().item()  # For printing after the validation loop.
-
-        # - Stores the output and the reference pixels to calculate the scores after inference on all the scenes.
-        outputs_flat = {chart: np.array([]) for chart in train_options['charts']}
-        inf_ys_flat = {chart: np.array([]) for chart in train_options['charts']}
-
-        net.eval()  # Set network to evaluation mode.
-        # - Loops though scenes in queue.
-        for inf_x, inf_y, masks, name in tqdm(
-            iterable=dataloader_val, total=len(train_options['validate_list']), colour='green', position=0
-        ):
-            torch.cuda.empty_cache()
-
-            # - Ensures that no gradients are calculated, which otherwise take up a lot of space on the GPU.
-            with torch.no_grad(), torch.cuda.amp.autocast():
-                inf_x = inf_x.to(device, non_blocking=True)
-                output = net(inf_x)
-
-            # - Final output layer, and storing of non masked pixels.
-            for chart in train_options['charts']:
-                output[chart] = torch.argmax(output[chart], dim=1).squeeze().cpu().numpy()
-                outputs_flat[chart] = np.append(outputs_flat[chart], output[chart][~masks[chart]])
-                inf_ys_flat[chart] = np.append(inf_ys_flat[chart], inf_y[chart][~masks[chart]].numpy())
-
-            del inf_x, inf_y, masks, output  # Free memory.
-
-        # - Compute the relevant scores.
-        combined_score, scores = compute_metrics(
-            true=inf_ys_flat, pred=outputs_flat, charts=train_options['charts'], metrics=train_options['chart_metric']
-        )
-
-        print("")
-        print(f"Final batch loss: {loss_batch:.3f}")
-        print(f"Epoch {epoch} score:")
-        for chart in train_options['charts']:
-            print(f"{chart} {train_options['chart_metric'][chart]['func'].__name__}: {scores[chart]}%")
-        print(f"Combined score: {combined_score}%")
-
-        # If the scores is better than the previous epoch, then save the model and rename the image to best_validation.
-        if combined_score > best_combined_score:
-            best_combined_score = combined_score
-            torch.save(
-                obj={
-                    'model_state_dict': net.state_dict(),
-                    'optimizer_state_dict': optimizer.state_dict(),
-                    'epoch': epoch,
-                },
-                f='best_model',
+            # - Compute the relevant scores.
+            combined_score, scores = compute_metrics(
+                true=inf_ys_flat,
+                pred=outputs_flat,
+                charts=train_options['charts'],
+                metrics=train_options['chart_metric'],
             )
-        del inf_ys_flat, outputs_flat  # Free memory.
+
+            print(f'Epoch last batch loss: {loss_batch:.3f}')
+            mlflow.log_metric('epoch_last_batch_loss', loss_batch, step=epoch)
+            print(f'\nEpoch {epoch} score:')
+            for chart in train_options['charts']:
+                print(f"{chart} {train_options['chart_metric'][chart]['func'].__name__}: {scores[chart]}%")
+            print(f'Combined score: {combined_score}%\n')
+            mlflow.log_metrics({f'{chart}_val': scores[chart] for chart in scores}, step=epoch)
+            mlflow.log_metric('epoch_combined_score_val', combined_score, step=epoch)
+
+            # If the scores is better than the previous epoch, then save the u and rename the image to best_validation.
+            if combined_score > best_combined_score:
+                best_combined_score = combined_score
+                torch.save(
+                    {
+                        'model_state_dict': net.state_dict(),
+                        'optimizer_state_dict': optimizer.state_dict(),
+                        'epoch': epoch,
+                    },
+                    best_model_artifact_path,
+                )
+            del inf_ys_flat, outputs_flat  # Free memory.
+        mlflow.log_artifact(best_model_artifact_path)
 
 
 if __name__ == '__main__':
-    main()
+    clize.run(main)
